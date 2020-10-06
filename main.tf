b58fcf807231c2db724db597f5d8f83f94dd5ea1
jonathanmorais <jonathan.lucena@zoom.com.br>
2020-10-06 11:41:11 -0300

    broken line

--- main.tf
+++ main.tf
@@ -1,9 +1,7 @@
 resource "aws_sqs_queue" "this" {
   count = var.create ? 1 : 0
-
   name        = var.name
   name_prefix = var.name_prefix
-
   visibility_timeout_seconds  = var.visibility_timeout_seconds
   message_retention_seconds   = var.message_retention_seconds
   max_message_size            = var.max_message_size
